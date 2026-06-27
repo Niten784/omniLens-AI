b208b2eb014a02bde2a3c762a353b1d6702f351b
Niten784 <134975497+Niten784@users.noreply.github.com>
2026-06-27 22:55:00 +0530

    Add files via upload

---
+++ metro.config.js
@@ -0,0 +1,5 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+config.resolver.blockList = [/\.cache\/openid-client\/.*/];
+module.exports = config;
