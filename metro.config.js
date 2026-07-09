ca1fd0c959a2c96124d1e29bbaaf84a88163e63c
Niten784 <134975497+Niten784@users.noreply.github.com>
2026-07-09 12:29:19 +0530

    OmniLens AI: full app with live camera features

--- metro.config.js
+++
@@ -1,5 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-config.resolver.blockList = [/\.cache\/openid-client\/.*/];
-module.exports = config;
